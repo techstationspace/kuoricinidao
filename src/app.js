f150f01a64e69dbc097bb847679ea6de82885693
Fabian Chatwin <fabianchatwin@gmail.com>
2022-03-19 11:41:11 +0100

    js for the video

--- src/app.js
+++ src/app.js
@@ -1,57 +1,28 @@
-App = {
+async function initWeb3() {
+  web3 = new Web3(window.ethereum);
 
-    initWeb3: async function() {
-      if (window.ethereum) {
-        web3Provider = window.ethereum;
-        try {
-          await window.ethereum.request({ method: "eth_requestAccounts" });;
-        } catch (error) {
-          console.error("User denied account access")
-        }
-      }
-      web3 = new Web3(web3Provider);
+  $.getJSON('KuoriciniDao.json', function(data) {
+    KuoriciniDao = TruffleContract(data);
+    KuoriciniDao.setProvider(window.ethereum);
+  });
 
-      $.getJSON('KuoriciniDao.json', function(data) {
-        KuoriciniDao = TruffleContract(data);
-        KuoriciniDao.setProvider(web3Provider);
-      });
-      return App.readAccount();
-    },
+  accounts = await web3.eth.getAccounts();
+  $('#myAddress').text(accounts[0]);
+  instance = await KuoriciniDao.deployed();
+  balance = await instance.balanceOf(accounts[0], {from: accounts[0]});
+  $('#myKuori').text(balance);
 
-    readAccount: function() {
-        web3.eth.getAccounts(function(error, accounts) {
-            $('#myAddress').text(accounts[0]);            
-            KuoriciniDao.deployed().then(function(instance) {
-                return instance.balanceOf(accounts[0], {from: accounts[0]});
-            }).then(function(result) {
-                $('#myKuori').text(result);
-            }).catch(function(err) {
-                alert("error");
-                console.log(err.message);
-            });
-        });
-      return App.bindEvents();      
-    },
+}
 
-    bindEvents: function(){
-      $(document).on('click', "#sendKuoriButton", App.sendKuori);
-    }, 
+initWeb3();
+$(document).on('click', "#sendKuoriButton", sendKuori);
 
-    sendKuori: function(){
-        sendAddress=$("#sendKuoriAddress").val();
-        web3.eth.getAccounts(function(error, accounts) {
-            KuoriciniDao.deployed().then(function(instance) {
-                return instance.transfer(sendAddress, 1, {from: accounts[0]});
-            }).then(function(result) {
-                App.readContract();
-            }).catch(function(err) {
-                alert("error");
-                console.log(err.message);
-            });
-        });
-    }
-};
+async function sendKuori() {
+  sendAddress=$("#sendKuoriAddress").val();
+  await instance.transfer(sendAddress, 1, {from: accounts[0]});
+}
 
-$(window).on('load', function() {
-  App.initWeb3();
-});
+
+//$(window).on('load',function() {
+//  App.initWeb3();
+//});
