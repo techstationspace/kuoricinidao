d60a80b4eeee9be1cb8c47ada71adb1d1066e66a
Fabian Chatwin <fabianchatwin@gmail.com>
2022-04-14 18:40:44 +0700

    EP4 completed

--- contracts/KuoriciniDao.sol
+++ contracts/KuoriciniDao.sol
@@ -2,7 +2,16 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract KuoriciniDao {
+
+struct DaoGroup {
+  string name;
+  address[] members;
+}
+
+DaoGroup[] daoGroups;
+
 mapping (address => uint) balances;
+mapping (address => string) names;
 string public symbol="KUORI";
 event Transfer(address indexed from, address indexed to, uint value);
 
@@ -10,8 +19,31 @@ constructor() public {
   balances[msg.sender] = 10;
 }
 
+function createGroup(string calldata groupName) public returns(bool){
+  DaoGroup memory newGroup;
+  newGroup.name=groupName;
+  address[] memory _members = new address[](1);
+  _members[0]=msg.sender;
+  newGroup.members=_members;
+  daoGroups.push(newGroup);
+  return true;
+}
+
+function allGroups() public view returns(DaoGroup[] memory){
+  return daoGroups;
+} 
+
 function balanceOf(address owner) public view returns(uint) {
   return balances[owner];
+}
+
+function nameOf(address owner) public view returns(string memory) {
+  return names[owner];
+}
+
+function setName(string calldata name) public returns(bool){
+  names[msg.sender]=name;
+  return true;
 }
 
 function transfer(address to, uint value) public returns(bool) {
--- src/app.js
+++ src/app.js
@@ -12,16 +12,34 @@ $("#myAddress").text(accounts[0]);
 
 instance = await KuoriciniDao.deployed();
 bal = await instance.balanceOf(accounts[0] , {from: accounts[0]});
+n = await instance.nameOf(accounts[0] , {from: accounts[0]});
+allgroups = await instance.allGroups({from: accounts[0]});
+
 
 $("#myKuori").text(bal);
+$("#myName").text(n);
+$("#theGroups").text(allgroups);
+
 
 }
 
 async function sendKuori() {
     to = $("#sendAddress").val();
     await instance.transfer(to, 1 , {from: accounts[0]});
+}
 
+async function setName() {
+    n = $("#setName").val();
+    await instance.setName(n , {from: accounts[0]});
+}
+
+async function newGroup() {
+    g = $("#newGroupName").val();
+    await instance.createGroup(g , {from: accounts[0]});
 }
 
 init();
 $(document).on("click", "#sendKuori", sendKuori);
+$(document).on("click", "#setNameButton", setName);
+$(document).on("click", "#newGroupButton", newGroup);
+
