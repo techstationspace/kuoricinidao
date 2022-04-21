cc4bd26f3ab29574a65098980f9d5b84370d4fc8
Fabian Chatwin <fabianchatwin@gmail.com>
2022-04-21 22:01:54 +0700

    ep5 ks click problem

--- contracts/KuoriciniDao.sol
+++ contracts/KuoriciniDao.sol
@@ -12,10 +12,12 @@ DaoGroup[] daoGroups;
 
 mapping (address => uint) balances;
 mapping (address => string) names;
+mapping (address => uint[] ) userGroups;
+
 string public symbol="KUORI";
 event Transfer(address indexed from, address indexed to, uint value);
 
-constructor() public {
+constructor() {
   balances[msg.sender] = 10;
 }
 
@@ -26,12 +28,39 @@ function createGroup(string calldata groupName) public returns(bool){
   _members[0]=msg.sender;
   newGroup.members=_members;
   daoGroups.push(newGroup);
+  userGroups[msg.sender].push(daoGroups.length-1);
   return true;
 }
 
 function allGroups() public view returns(DaoGroup[] memory){
   return daoGroups;
 } 
+
+function group(uint gid) public view returns(DaoGroup memory){
+  require(isAddressInGroup(msg.sender, gid), "user not authorized"); 
+  return daoGroups[gid];
+}
+
+function myGroups() public view returns(uint[] memory) {
+  return userGroups[msg.sender];
+}
+
+function addAddresstoGroup(address addr, uint gid) public returns(bool){
+  require(isAddressInGroup(msg.sender, gid), "user not authorized"); 
+  require(!isAddressInGroup(addr, gid), "address already present"); 
+  daoGroups[gid].members.push(addr);
+  userGroups[addr].push(gid);
+  return true;
+} 
+
+function isAddressInGroup(address addr, uint gid) private view returns(bool) {
+  for ( uint i = 0; i < daoGroups[gid].members.length; i++){
+    if ( daoGroups[gid].members[i] == addr ){
+      return true;
+    }
+  }
+  return false;
+}
 
 function balanceOf(address owner) public view returns(uint) {
   return balances[owner];
--- src/app.js
+++ src/app.js
@@ -1,27 +1,44 @@
-
 async function init() {
-web3 = new Web3(window.ethereum);
+    web3 = new Web3(window.ethereum);
 
-$.getJSON("KuoriciniDao.json", function(data){
-    KuoriciniDao = TruffleContract(data);
-    KuoriciniDao.setProvider(window.ethereum);
-});
+    $.getJSON("KuoriciniDao.json", function(data){
+        KuoriciniDao = TruffleContract(data);
+        KuoriciniDao.setProvider(window.ethereum);
+    });
 
-accounts = await web3.eth.getAccounts();
-$("#myAddress").text(accounts[0]);
+    accounts = await web3.eth.getAccounts();
+    $("#myAddress").text(accounts[0]);
 
-instance = await KuoriciniDao.deployed();
-bal = await instance.balanceOf(accounts[0] , {from: accounts[0]});
-n = await instance.nameOf(accounts[0] , {from: accounts[0]});
-allgroups = await instance.allGroups({from: accounts[0]});
+    instance = await KuoriciniDao.deployed();
+    bal = await instance.balanceOf(accounts[0] , {from: accounts[0]});
+    n = await instance.nameOf(accounts[0] , {from: accounts[0]});
+    gids = await instance.myGroups({from: accounts[0]});
 
+    $("#myKuori").text(bal);
+    $("#myName").text(n);
+    $("#theGroups").text(gids);
 
-$("#myKuori").text(bal);
-$("#myName").text(n);
-$("#theGroups").text(allgroups);
+    table = document.getElementById("myGroupsTable")
+    table.innerHTML="";
+    for (gid in gids) {
+            row=table.insertRow(-1);
+            cell=row.insertCell(0);
+            cell.innerHTML=gid;
+            group = await instance.group(gid, {from: accounts[0]});
+            cell=row.insertCell(-1);
+            cell.innerHTML=group.name;
+            cell=row.insertCell(-1);
+            cell.innerHTML=group.members.length;
+            $(row).click(function() {showGroup(group.name)});
+            alert(row);            
+    }
+}
 
+async function showGroup(g) {
+    alert(g);
 
 }
+
 
 async function sendKuori() {
     to = $("#sendAddress").val();
