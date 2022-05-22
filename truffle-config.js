cfcf0a8329ae6783248ef7036115bf86db678b33
Fabian Chatwin <fabianchatwin@gmail.com>
2022-05-22 12:08:15 +0700

    experiment matic

--- truffle-config.js
+++ truffle-config.js
@@ -17,7 +17,7 @@
  * phrase from a file you've .gitignored so it doesn't accidentally become public.
  *
  */
-const { projectId, mnemonic } = require('./secrets.json');
+const { projectId, mnemonic } = require('./secret-matic.json');
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 //
 // const fs = require('fs');
@@ -43,9 +43,9 @@ module.exports = {
     //
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
-//      host: "192.168.80.1",     // Localhost (default: none)
-//      port: 7545,            // Standard Ethereum port (default: none)
+//     port: 8545,            // Standard Ethereum port (default: none)
+//      host: "192.168.1.6",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
      },
 
@@ -61,11 +61,37 @@ module.exports = {
     ropsten: {
       provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${projectId}`),
       network_id: 3,       // Ropsten's id
-      gas: 3000000,        // Ropsten has a lower block limit than mainnet
+      networkCheckTimeout: 999999,                   
+      gas: 6000000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },    
+    
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, `https://kovan.infura.io/v3/${projectId}`);
+      },
+      network_id: 42,
+      networkCheckTimeout: 999999
+    },
+    
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, `wss://rinkeby.infura.io/v3/${projectId}`);
+      },
+      network_id: 4,
+      networkCheckTimeout: 999999      
+    },
+
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.maticvigil.com`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    }    
+    
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
