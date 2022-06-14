cdce78af417b90394b5b689c02e401c02e9cee1a
fabian <fabianchatwin@gmail.com>
2022-06-14 10:22:13 +0700

    try polygon truffle package

--- truffle-config.js
+++ truffle-config.js
@@ -17,16 +17,24 @@
  * phrase from a file you've .gitignored so it doesn't accidentally become public.
  *
  */
-//const { projectId, mnemonic } = require('./secret-matic.json');
 
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret-matic").toString().trim();
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
+// const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
-module.exports = {
+module.exports = {  /**
+
+  * contracts_build_directory tells Truffle where to store compiled contracts
+  */
+  contracts_build_directory: './build/ethereum-contracts',
+
+  /**
+  * contracts_directory tells Truffle where the contracts you want to compile are located
+  */
+  contracts_directory: './contracts/ethereum',
+
   /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
@@ -37,92 +45,13 @@ module.exports = {
    * $ truffle test --network <network-name>
    */
 
-  networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-//     port: 8545,            // Standard Ethereum port (default: none)
-//      host: "192.168.1.6",     // Localhost (default: none)
-      port: 7545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-//      gasPrice: 1,
-      gas: 6721975,
-     },
-
-     goerli: {
-      provider: () => {
-        return new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${projectId}`)
-      },
-      network_id: '5', // eslint-disable-line camelcase
-      gas: 4465030,
-      gasPrice: 10000000000,
-    },
-
-    ropsten: {
-      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${projectId}`),
-      network_id: 3,       // Ropsten's id
-      networkCheckTimeout: 999999,                   
-      gas: 6000000,        // Ropsten has a lower block limit than mainnet
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    },    
-    
-    kovan: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, `https://kovan.infura.io/v3/${projectId}`);
-      },
-      network_id: 42,
-      networkCheckTimeout: 999999
-    },
-    
-    rinkeby: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, `wss://rinkeby.infura.io/v3/${projectId}`);
-      },
-      network_id: 4,
-      networkCheckTimeout: 999999      
-    },
-
-    matic: {
-      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.maticvigil.com`),
-      network_id: 80001,
-      confirmations: 2,
-      timeoutBlocks: 200,
-      skipDryRun: true
-    }    
-    
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websocket: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
-  },
+   networks: {
+     development: {
+       host: "127.0.0.1",     // Localhost (default: none)
+       port: 8545,            // Standard Ethereum port (default: none)
+       network_id: "*",       // Any network (default: none)
+     }
+   },
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
@@ -132,7 +61,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.11",    // Fetch exact version from solc-bin (default: truffle's version)
+      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
@@ -144,24 +73,9 @@ module.exports = {
     }
   },
 
-  // Truffle DB is currently disabled by default; to enable it, change enabled:
-  // false to enabled: true. The default storage location can also be
-  // overridden by specifying the adapter settings, as shown in the commented code below.
-  //
-  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
-  // make a backup of your artifacts to a safe location before enabling this feature.
-  //
-  // After you backed up your artifacts you can utilize db by running migrate as follows: 
-  // $ truffle migrate --reset --compile-all
-  //
-  // db: {
-    // enabled: false,
-    // host: "127.0.0.1",
-    // adapter: {
-    //   name: "sqlite",
-    //   settings: {
-    //     directory: ".db"
-    //   }
-    // }
-  // }
+  // Truffle DB is enabled in this project by default. Enabling Truffle DB surfaces access to the @truffle/db package
+  // for querying data about the contracts, deployments, and networks in this project
+  db: {
+    enabled: true
+  }
 };
